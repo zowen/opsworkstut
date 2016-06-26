3149d7a650442895dec33bbe297ed66ea74e71a0
zowen <zowen@mit.edu>
2016-06-26 10:36:01 -0400

    trying to work with deploy

--- opsworks-linux-demo-cookbook-django/recipes/default.rb
+++ opsworks-linux-demo-cookbook-django/recipes/default.rb
@@ -13,9 +13,18 @@ package 'git' do
   options '--force-yes' if node['platform'] == 'ubuntu' && node['platform_version'] == '14.04'
 end
 
+#prepare_git_checkouts(
+#  user: deploy[:user],
+#  group: deploy[:group],
+#  home: deploy[:home],
+#  ssh_key: "#{application['app_source']['ssh_key']}"
+#)
+
 application app_path do
   git app_path do
     repository app['app_source']['url']
+    deploy_key app['app_source']['ssh_key']
+    #node[:deploy]["#{app['shortname']}"][:scm][:ssh_key]
     action :sync
   end
 
